daeca5f4dfb67a178511a9b6032f1b1c3524588f
kandy <kandypeter03@gmail.com>
2023-01-06 22:56:49 +0200

    Add redis and new_room_form with broadcast update after commit.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,23 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @room = Room.new
     @rooms = Room.public_rooms
     @users = User.all_except(current_user)
+  end
+
+  def create
+    @room = Room.create(room_params)
+    if @room.save
+      flash[:notice] = "Room created successfully" 
+    else
+      redirect_to rooms_path
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,6 @@ class Room < ApplicationRecord
   validates_uniqueness_of :name
   scope :public_rooms, -> { where(is_private: false) }
   scope :private_rooms, -> { where(is_private: true) }
+
+  after_create_commit { broadcast_append_to "rooms" }
 end
