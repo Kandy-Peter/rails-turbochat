b0fd2ab3c8b05bd449b37a61b69ddee865ada15f
kandy <kandypeter03@gmail.com>
2023-01-07 13:05:26 +0200

    Add private messages between diff participants

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,13 @@ class Message < ApplicationRecord
   belongs_to :room
 
   after_create_commit { broadcast_append_to self.room }
+
+  before_create :confirm_participant
+
+  def confirm_participant
+    return unless room.is_private
+
+    is_participant = Participant.where(user_id: self.user.id, room_id: self.room.id).first
+    throw :abort unless is_participant
+  end
 end
